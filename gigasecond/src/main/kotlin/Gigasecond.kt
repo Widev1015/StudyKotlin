96f7e64bf5ded0f9f45b95a814ddb28c1c3bc088
WidevMac <widev1015@gmail.com>
2021-12-18 19:39:54 +0900

    first commit

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,8 +1,19 @@
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 
 class Gigasecond {
+    val date: LocalDateTime
 
-    // TODO: Implement proper constructor
+    var longSecond = 1000000000
 
-    val date: LocalDateTime = TODO("Implement this getter to complete the task")
+    constructor(ld: LocalDate){
+        date = LocalDateTime.of(ld, LocalTime.MIN).plus(longSecond.toLong(), ChronoUnit.SECONDS)
+    }
+
+    constructor(ldt: LocalDateTime){
+        date = ldt.plus(longSecond.toLong(), ChronoUnit.SECONDS)
+    }
 }
+
